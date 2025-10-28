40184ca50e27e5b29629408ad9beaa3fe9a99e33
raccoon-coding <minsungt@naver.com>
2025-10-28 11:05:24 +0900

    [REFA] 전체 배송 조회(웹소캣) 수정 완료

--- hperlinkMain/src/api/users/index.js
+++ hperlinkMain/src/api/users/index.js
@@ -166,6 +166,17 @@ export const deleteDriver = async (id) => {
     }
 };
 
+export const getStoreWithAddress = async () => {
+    try {
+        const response = await api.get(`/api/member/store/list/address`);
+        return { success: true, data: response.data.data };
+    } catch (error) {
+        const errorData = error.response?.data || { message: '네트워크 오류 또는 서버 응답 없음' };
+        errorData.success = false;
+        return errorData;
+    }
+};
+
 export default {
     getDrivers,
     getStoresWithPos,
@@ -182,4 +193,5 @@ export default {
     deleteStore,
     deletePos,
     deleteDriver,
+    getStoreWithAddress,
 };
