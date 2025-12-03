835e9ce81b1e74493959d46a3ef836d5b1762448
raccon <minsungt@naver.com>
2025-12-03 23:02:16 +0900

    [REFA] URL 수정

--- hypelinkMain/src/api/item/store/index.js
+++ hypelinkMain/src/api/item/store/index.js
@@ -2,7 +2,7 @@ import api from "@/plugins/axiosinterceptor";
 
 export const getItems = async (page = 0, size = 10, sort = 'id,desc',
                                storeId, keyWord = '', category = 'all') => {
-    const requestUrl = `/api/store/item/purchase/list`
+    const requestUrl = `/api/direct/store/item/purchase/list`
     let data = {}
     await api.get(requestUrl, {
         params: { page, size, sort, storeId, keyWord, category },
